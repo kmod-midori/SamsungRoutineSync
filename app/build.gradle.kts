59ee9abaeb8a0378ecf15cff398674d314ee0ba4
Midori Kochiya <i@reimu.moe>
2026-03-14 22:59:32 +0800

    Deps clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,6 @@ android {
         targetSdk = 36
         versionCode = 490420000
         versionName = "4.9.04.20"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -52,11 +50,6 @@ dependencies {
 
     implementation(libs.taskerpluginlibrary)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
